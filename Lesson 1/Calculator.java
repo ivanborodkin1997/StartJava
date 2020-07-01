89d1dac8884d25c77eda49ab9b26ac0e4f8eebf0
ivanborodkin1997 <47855178+ivanborodkin1997@users.noreply.github.com>
2020-07-01 15:36:33 +0300

    Add files via upload

---
+++ Lesson 1/Calculator.java
@@ -0,0 +1,24 @@
+public class Calculator {
+	public static void main(String[] args) {
+		int a = 7, b = 3, c = 343;
+
+		if(a + b == c ) {
+			System.out.println(c);
+		}
+		else if(a - b == c) {
+			System.out.println(c);
+		}
+		else if(a * b == c) {
+			System.out.println(c);
+		}
+		else if(a / b == c) {
+			System.out.println(c);
+		}
+		else if(a * a * a  == c) {
+			System.out.println(c);
+		}
+		else if(a % b == c) {
+			System.out.println(c);
+		}
+	}
+}
