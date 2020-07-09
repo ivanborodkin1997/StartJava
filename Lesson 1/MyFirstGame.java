c18839a054c0830351338a2f50cbd346fd125292
ivanborodkin1997 <47855178+ivanborodkin1997@users.noreply.github.com>
2020-07-09 23:11:13 +0300

    Add files via upload

---
+++ Lesson 1/MyFirstGame.java
@@ -0,0 +1,18 @@
+public class MyFirstGame {
+	public static void main(String[] args) {
+		int computerNumber = 57 ;  // число загаданное компьютером
+		int playerNumber = 55;  // число введенное человеком
+		
+		do {
+			if(computerNumber < playerNumber) {
+				System.out.println("Введенное вами число больше того, что загадал компьютер");
+				playerNumber--;
+			}
+			else if(computerNumber > playerNumber) {
+				System.out.println("Введенное вами число меньше того, что загадал компьютер");
+				playerNumber++;
+			}	
+		} while(computerNumber != playerNumber);
+		System.out.println("Вы угадали число!");                           
+	}
+}
