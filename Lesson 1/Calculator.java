bbbe74e251f7c4ce1c7812cebdf124fdc60e2c3c
ivanborodkin1997 <47855178+ivanborodkin1997@users.noreply.github.com>
2020-07-09 18:51:55 +0300

    Add files via upload

---
+++ Lesson 1/Calculator.java
@@ -0,0 +1,25 @@
+public class Calculator {
+	public static void main(String[] args) {
+		int firstNumber = 7;
+		int secondNumber = 3;
+		char sign = '^';
+
+		if(sign == '+') {
+			System.out.println(firstNumber + secondNumber);
+		} else if(sign == '-') {
+			System.out.println(firstNumber - secondNumber);
+		} else if(sign == '*') {
+			System.out.println(firstNumber * secondNumber);
+		} else if(sign == '/') {
+			System.out.println(firstNumber / secondNumber);
+		} else if(sign == '^') {
+			int numberInDegree = 1;
+			for(int i = 0; i < secondNumber; i++) {
+				numberInDegree *= firstNumber;
+			}
+			System.out.println(numberInDegree);    	
+		} else if(sign == '%') {
+			System.out.println(firstNumber % secondNumber);
+		}
+	}
+}
