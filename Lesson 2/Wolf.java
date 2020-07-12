f7b95b11794b2a26e6913b4e63dad5f6636e8189
ivanborodkin1997 <47855178+ivanborodkin1997@users.noreply.github.com>
2020-07-12 17:47:26 +0300

    Add files via upload

---
+++ Lesson 2/Wolf.java
@@ -0,0 +1,29 @@
+public class Wolf {
+
+	String gender = "man";
+	String name = "Nord";
+	float weigth = 1.3f;
+	int age = 8;
+	String  color = "Black";
+
+	void go() {
+		System.out.println("Иди спокойно!");
+	}
+
+	void sit() {
+		System.out.println("Остановись и посиди, путник");
+	}
+
+	void run() {
+		System.out.println("Вы пробежали 5 км");
+	}
+
+	void howl() {
+		System.out.println("Видишь луну? Начинй выть!");
+	}
+
+	void hunt() {
+		System.out.println("Добудь себе еды");
+	}
+
+}
