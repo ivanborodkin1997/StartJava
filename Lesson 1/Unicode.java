74348dd6981d6fdac3cf4dc646253881ebb99cc8
ivanborodkin97 <boroda0360@mail.ru>
2020-08-18 14:56:22 +0300

    All files in one folder

--- Lesson 1/Unicode.java
+++
@@ -1,9 +0,0 @@
-public class Unicode {
-	public static void main(String[] args) {
-	 char symbol = 9398;
-	 
-        while (symbol != 10178) {
-             System.out.println(symbol++);
-        }
-    }
-}
