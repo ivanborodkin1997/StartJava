1e9ca576e10898ff34740ba8ced589a7fb6001f6
ivanborodkin97 <boroda0360@mail.ru>
2020-08-27 22:35:59 +0300

    I did it!

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -1,24 +1,24 @@
 package com.startjava.lesson_2_3_4.calculator;
 
 public class Calculator {
-	public int calculate(int[] numbers, char sign) {
+	public int calculate(int[] numbers, String sign) {
 		switch (sign) {
-			case '+' : 
+			case "+" :
 				numbers[2] = Math.addExact (numbers[0], numbers[1]);
 				break;
-			case '-' :
+			case "-" :
 				numbers[2]= Math.subtractExact(numbers[0],numbers[1]);
 				break;
-			case '*' :
+			case "*" :
 				numbers[2] = Math.multiplyExact(numbers[0], numbers[1]);
 				break;
-			case '/' :
+			case "/" :
 				numbers[2] = Math.floorDiv (numbers[0],numbers[1]);
 				break;
-			case '^' :
+			case "^" :
 				numbers[2] = (int) Math.pow (numbers[0],numbers[1]);
 				break;   	
-			case '%':
+			case "%":
 				numbers[2]= (numbers[0] % numbers[1]);
 				break;
 		}
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -1,5 +1,6 @@
 package com.startjava.lesson_2_3_4.calculator;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class CalculatorTest {
@@ -8,26 +9,27 @@ public class CalculatorTest {
 		String yesOrNo;
 		System.out.println("Калькулятор запущен!");
 		int[] numbers = new int[3];
+
 		
 		do {
 			Calculator calculator = new Calculator();
 			Scanner scanner = new Scanner(System.in);
-				
-			System.out.print("\nВведите первое число : ");
-			numbers[0] = scanner.nextInt();
-			System.out.print("Введите знак математической операции : ");
-			char sign = scanner.next().charAt(0);
-			System.out.print("Введите второе число : ");
-			numbers[1] = scanner.nextInt();
+
+			System.out.print("Введите математическое выражение: ");
+			String example = scanner.nextLine();
+			String[] array = example.split(" ");
+			numbers[0] = Integer.parseInt(array[0]);
+			String sign = array[1];
+			numbers[1] = Integer.parseInt(array[2]);
+
 			System.out.print("Результат вычисления : ");
 
-
 			System.out.println(calculator.calculate(numbers, sign));
-			System.out.println();
-			scanner.nextLine();
+
 			do {
 				System.out.print("Хотите продолжить? [да/нет] : ");
 				yesOrNo = scanner.nextLine();
+				System.out.println();
 		 	} while(!yesOrNo.equalsIgnoreCase("yes") && !yesOrNo.equalsIgnoreCase("no"));
 		} while(yesOrNo.equalsIgnoreCase("yes"));
 	}
