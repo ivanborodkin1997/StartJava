e2e4cffa90aabb82d3e0fed33cbf1128f1fe0d77
ivanborodkin1997 <boroda0360@mail.ru>
2020-11-06 13:35:03 +0300

    Lesson 5

---
+++ src/com/startjava/lesson_5/queries.sql
@@ -0,0 +1,21 @@
+--queries
+
+SELECT * FROM Jeagers ORDER BY id; --вывод всей таблицы, в порядке возрастания id
+
+SELECT * FROM Jeagers WHERE status = 'Active'; --отображение только не уничтоженных роботов
+
+SELECT * FROM Jeagers WHERE mark IN ('1','6') ORDER BY id; --отображение роботов нескольких серий (Mark-1,6)
+
+SELECT * FROM Jeagers ORDER BY mark DESC; --отсортированная таблицы по убыванию по столбцу mark
+
+SELECT * FROM Jeagers WHERE launch = (SELECT MIN(launch) FROM Jeagers); --самый старый робот
+
+SELECT * FROM Jeagers WHERE kaijukill = (SELECT MAX(kaijukill) FROM Jeagers); --убил больше всех kaiju
+
+SELECT * FROM Jeagers WHERE kaijukill = (SELECT MIN(kaijukill) FROM Jeagers); --убил меньше всех kaiju
+
+SELECT AVG(weight) FROM Jeagers; --средний вес роботов
+
+UPDATE Jeagers SET kaijukill = kaijukill + 1 WHERE status = 'Active'; --увеличил на единицу количество уничтоженных kaiju у роботов, которые до сих пор не разрушены
+
+DELETE FROM Jeagers WHERE status = 'Distroyed'; --удалил уничтоженных роботов
